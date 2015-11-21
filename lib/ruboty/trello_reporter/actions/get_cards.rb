5cd004a18767ecb18c19bfd39784bd306052bb8a
izumin5210 <masayuki@izumin.info>
2015-11-22 02:00:29 +0900

    Implement GetCards action

---
+++ lib/ruboty/trello_reporter/actions/get_cards.rb
@@ -0,0 +1,17 @@
+module Ruboty
+  module TrelloReporter
+    module Actions
+      class GetCards < Base
+        def call
+          grouped_cards = cards.inject(Hash.new { |h, k| h[k] = [] }) do |grouped, card|
+                card.labels.each do |label|
+                  grouped[label.name] << card.name
+                end
+                grouped
+              end
+          message.reply(grouped_cards.map { |l, cs| "#{l}\n#{cs.map { |c| "* #{c}" }.join("\n")}" }.join("\n\n"))
+        end
+      end
+    end
+  end
+end
